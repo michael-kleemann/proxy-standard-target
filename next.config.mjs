e808b68a0f34a7b56b62717b4f1c6035741399ff
Michael Kleemann <michael.kleemann@seven.one>
2024-08-20 15:47:32 +0200

    change env

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     const rootPath = '/bts';
     const subPath = '/bts/:path*';
     const reverseProxyUrl =
-      'https://feature-pub-5121-clientside-routing.feat.bts.joyn.de';
+      'bugfix-pub-0-500-errors.feat.bts.joyn.de';
 
     const reverseProxyRootUrl = `${reverseProxyUrl}${rootPath}`;
     const reverseProxySubUrl = `${reverseProxyUrl}${subPath}`;
