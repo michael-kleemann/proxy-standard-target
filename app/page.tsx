f0cd06229a4a9b1f255522c1caecefdc8bcc9568
Michael Kleemann <michael.kleemann@seven.one>
2024-08-23 08:02:30 +0200

    showcase page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image';
+import Link from "next/link";
 
 export default function Home() {
+
+  const randomID = Math.floor(Math.random() * 256);
+
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <h1 className="text-6xl font-bold ">Joyn</h1>
+    <main className="flex min-h-screen items-center justify-center gap-24 p-24">
+      <h1 className="text-6xl font-bold ">Main Page</h1>
+      <Link href={`/${randomID}`} prefetch={true}>Link to Subpage {randomID}</Link>
     </main>
   );
 }
